53c8d6a2ffb7fb1a661e8e137ea02ecc46e939a0
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-10-03 22:38:36 +0200

    refactor Work and Skill Component

--- src/Components/MyWorks/Work/Work.jsx
+++ src/Components/MyWorks/Work/Work.jsx
@@ -4,18 +4,13 @@ import style from './Work.module.css'
 const Work = () => {
     return (
         <div className={style.workBox}>
-            <div className={style.imgBox}>
-                    <div className={style.wrap}>
-                        <button className={style.workButton}>Watch</button>
-                        <div className={style.img}>
-                            <h3 className={style.title}>TodoList</h3>
-                            <p className={style.description}>As its name implies, the To-do list on an article's talk page shows the list of improvements suggested for the article.</p>
-                        </div>
-
-
-                    </div>
-            </div>
-            <div className={style.workInfo}>
+            <div className={style.wrap}>
+                <div className={style.img}>
+                    <h3 className={style.title}>TodoList</h3>
+                    <p className={style.description}>As its name implies, the To-do list on an article's talk page shows
+                        the list of improvements suggested for the article.</p>
+                    <button className={style.workButton}>Watch</button>
+                </div>
             </div>
         </div>
     );
