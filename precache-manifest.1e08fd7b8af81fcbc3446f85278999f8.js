e69ef27f2926502d29f9c77caaf7b559f0ea7718
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-10-10 16:21:42 +0200

    Updates

---
+++ precache-manifest.1e08fd7b8af81fcbc3446f85278999f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549e8aaffd397fe4e5d0d524b3162df0",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "585fc40f6cace9dd69ef",
+    "url": "/My-Portfolio/static/css/main.e25fbbd0.chunk.css"
+  },
+  {
+    "revision": "1748a10daa09c65e1097",
+    "url": "/My-Portfolio/static/js/2.89f63e48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Portfolio/static/js/2.89f63e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585fc40f6cace9dd69ef",
+    "url": "/My-Portfolio/static/js/main.1699996a.chunk.js"
+  },
+  {
+    "revision": "cc8fb5c09ef196e1df09",
+    "url": "/My-Portfolio/static/js/runtime-main.2dca773a.js"
+  },
+  {
+    "revision": "4a197bf7069d81022a8d4c2babf7308e",
+    "url": "/My-Portfolio/static/media/PortfPhoto.4a197bf7.jpg"
+  }
+]);
