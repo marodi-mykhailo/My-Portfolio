d1399c20ef3ba20a753e5ca9f83cc83b7b0c4514
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-10-14 21:14:19 +0200

    change hover for Work at mobile devices

--- src/Components/MyInfo/MyInfo.jsx
+++ src/Components/MyInfo/MyInfo.jsx
@@ -6,12 +6,12 @@ const MyInfo = () => {
     return (
         <section className={styles.myInfo}>
             <div className={styles.infoBlock}>
-                <p>Hi there 😊</p>
+                <p>Hi there <span role={'img'} aria-label={'shineFace'}>😊</span></p>
                 <h1>I am <span>Marodi Mykhailo</span></h1>
                 <p>Front-end developer</p>
             </div>
             <div className={styles.myPhoto}>
-                <img src={portfPhoto} alt={'My Photo'}/>
+                <img src={portfPhoto} alt={'MyPhoto'}/>
             </div>
         </section>
     );
